6d2d3ac7b4975b123706c18e40e7f173196967d7
artempelevin <78497650+artempelevin@users.noreply.github.com>
2023-01-16 00:24:29 +0300

    feat: Спроектирован интерфейс хранилища данных прибора

---
+++ FluxViewer.DataAccess/Storage/IStorage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using FluxViewer.DataAccess.Models;
+
+namespace FluxViewer.DataAccess.Storage;
+
+/// <summary>
+/// Интерфейс взаимодействия с хранилищем показаний прибора.
+/// В нём доступны методы чтения/записи показаний.
+/// </summary>
+public interface IStorage
+{
+    /// <summary>
+    /// Открыть хранилище.
+    /// </summary>
+    void Open();
+
+    /// <summary>
+    /// Закрыть хранилище.
+    /// </summary>
+    void Close();
+
+    /// <summary>
+    /// Добавление показания прибора в хранилище.
+    /// </summary>
+    /// <param name="data">Структура, описывающая 1 показание прибора</param>
+    void WriteData(Data data);
+
+
+    /// <summary>
+    /// "Пакетное" получение показаний прибора между двумя датами из хранилища.
+    /// </summary>
+    /// <param name="beginDate">Дата начала, с которой следует искать показания</param>
+    /// <param name="endDate">Дата конца, по которую следует искать показания</param>
+    /// <param name="batchNumber">Номер пакета</param>
+    /// <param name="batchSize">Размер пакета</param>
+    /// <returns>Все показания прибора в текущий временной интервал</returns>
+    public List<Data> GetDataBatchBetweenTwoDates(DateTime beginDate, DateTime endDate, int batchNumber, int batchSize);
+}
