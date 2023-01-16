731165d50889382fd0dd4d7079bddc4450718c97
artempelevin <artempelevin@list.ru>
2023-01-16 16:00:34 +0300

    feat: Добавлены методы получения кол-ва показаний за всё время  и за промежуток между двумя датами

--- FluxViewer.DataAccess/Storage/IStorage.cs
+++ FluxViewer.DataAccess/Storage/IStorage.cs
@@ -26,6 +26,19 @@ public interface IStorage
     /// <param name="data">Структура, описывающая 1 показание прибора</param>
     void WriteData(Data data);
 
+    /// <summary>
+    /// Получение количества всех показаний, записанных за всё время.
+    /// </summary>
+    /// <returns>Количество показаний прибора</returns>
+    public int GetDataCount();
+    
+    /// <summary>
+    /// Получение количества показаний между двумя датами.
+    /// </summary>
+    /// <param name="beginDate">Дата начала, с которой считаются показания</param>
+    /// <param name="endDate">Дата конца, по которую считаются показания</param>
+    /// <returns>Количество показаний прибора</returns>
+    public int GetDataCountBetweenTwoDates(DateTime beginDate, DateTime endDate, int? step = null);
 
     /// <summary>
     /// "Пакетное" получение показаний прибора между двумя датами из хранилища.
