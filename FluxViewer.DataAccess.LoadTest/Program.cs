47d36bba9a9c077fe2a20b67ded3f153b1fee67d
artempelevin <78497650+artempelevin@users.noreply.github.com>
2023-01-25 00:45:46 +0300

    test: Немного изменёна функция создания файлов с данными

--- FluxViewer.DataAccess.LoadTest/Program.cs
+++ FluxViewer.DataAccess.LoadTest/Program.cs
@@ -1,37 +1,42 @@
 ﻿using FluxViewer.DataAccess.Models;
+using FluxViewer.DataAccess.Storage;
 
-void CreateFileWithRandomData(DateTime date, long dataCount)
+void CreateDataFiles(DateTime beginDate, DateTime endDate, float timeDeltaMilliseconds)
 {
     var pathToStorageDir = Path.Combine(Directory.GetCurrentDirectory(), "data");
     if (!Directory.Exists(pathToStorageDir))
         Directory.CreateDirectory(pathToStorageDir);
 
-    var filename = date.ToString("yyyy_MM_dd") + ".flux";
-    var pathToFile = Path.Combine(pathToStorageDir, filename);
-    using var file = new FileStream(pathToFile, FileMode.Append);
-
-    for (var i = 0; i < dataCount; i++)
+    var i = 0;
+    for (var dayDelta = 0; dayDelta < (endDate - beginDate).Days; dayDelta++)
     {
-        file.Write(new NewData(
-                i,
-                DateTime.Now,
-                (float)DateTime.Now.Millisecond % 20 / 100,
-                (float)DateTime.Now.Millisecond % 100 / 100,
-                (float)DateTime.Now.Millisecond % 10 / 100,
-                (float)DateTime.Now.Millisecond % 50 / 100
-            ).Serialize()
-        );
+        var currentDate = beginDate.AddDays(dayDelta);
+        var filename = currentDate.ToString("yyyy_MM_dd") + ".flux";
+        var pathToFile = Path.Combine(pathToStorageDir, filename);
+        using var file = new FileStream(pathToFile, FileMode.Append);
+
+        var nextDate = currentDate.AddDays(1);
+        while (currentDate < nextDate)
+        {
+            file.Write(new NewData(
+                    i++,
+                    currentDate,
+                    (float)currentDate.Millisecond % 20 / 100,
+                    (float)currentDate.Millisecond % 100 / 100,
+                    (float)currentDate.Millisecond % 10 / 100,
+                    (float)currentDate.Millisecond % 50 / 100
+                ).Serialize()
+            );
+            currentDate = currentDate.AddMilliseconds(timeDeltaMilliseconds);
+        }
     }
 }
 
 
 var beginDate = new DateTime(2022, 11, 24);
 var endDate = new DateTime(2023, 01, 15);
-const int dataCount = 216000;
-
-var currentDate = new DateTime(beginDate.Year, beginDate.Month, beginDate.Day);
-while (currentDate < endDate)
-{
-    CreateFileWithRandomData(currentDate, dataCount);
-    currentDate = currentDate.AddDays(1);
-}
+CreateDataFiles(beginDate, endDate, 400);
+var storage = new FileSystemStorage();
+storage.Open();
+Console.WriteLine(storage.GetDataCountBetweenTwoDates(new DateTime(2022, 11, 24), new DateTime(2022, 11, 24)));
+storage.Close();
