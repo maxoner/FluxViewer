44e67feca965b0136146582052f89ce70cd9ac32
artempelevin <78497650+artempelevin@users.noreply.github.com>
2023-02-01 22:13:13 +0300

    feat: Реализован статический метод генерации батча за целый день

---
+++ FluxViewer.DataAccess/Exporters/DataBatchGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using FluxViewer.DataAccess.Models;
+
+namespace FluxViewer.DataAccess.Exporters;
+
+public static class DataBatchGenerator
+{
+    /// <summary>
+    /// Генерирует фиксированные показания прибора за определённую дату.
+    /// Такое может потребоваться, для того, чтобы заполнить пробел длиной в день!
+    /// </summary>
+    /// <param name="date">Дата, за которую будут сгенерированны показания</param>
+    /// <param name="timeShift">Время (в мс.) между двумя показаниями прибора</param>
+    /// <param name="fixedFlux">Фиксированное электростатическое поле</param>
+    /// <param name="fixedTemp">Фиксированная температура</param>
+    /// <param name="fixedPres">Фиксированное давление</param>
+    /// <param name="fixedHumm">Фиксированная влажность</param>
+    /// <returns></returns>
+    public static List<NewData> GenerateDataBatch(DateTime date, float timeShift, float fixedFlux,
+        float fixedTemp, float fixedPres, float fixedHumm)
+    {
+        var dataBatch = new List<NewData>();
+        var currentDate = new DateTime(date.Year, date.Month, date.Day, 00, 00, 00);
+        var finishDate = currentDate.AddDays(1);
+        while (currentDate < finishDate)
+        {
+            dataBatch.Add(new NewData(currentDate, fixedFlux, fixedTemp, fixedPres, fixedHumm));
+            currentDate = currentDate.AddMilliseconds(timeShift);
+        }
+
+        return dataBatch;
+    }
+}
