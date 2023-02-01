05d91f614471ce21a5ef446d83cb2260ca38ef15
artempelevin <78497650+artempelevin@users.noreply.github.com>
2023-02-01 22:34:58 +0300

    feat: Добавлена вся логика со стороны UI для экспорта данных прибора в файлы

--- FluxViewer.App/Form1.cs
+++ FluxViewer.App/Form1.cs
@@ -1,7 +1,8 @@
 using System.Diagnostics;
+using System.Globalization;
 using System.IO.Ports;
 using System.Text;
-using FluxViewer.DataAccess.Converters;
+using FluxViewer.DataAccess.Exporters;
 using FluxViewer.DataAccess.LiteDbb;
 using FluxViewer.DataAccess.Models;
 using FluxViewer.DataAccess.Storage;
@@ -105,6 +106,9 @@ namespace FluxViewer
             // Дата начала и окончания по умолчанию текущие
             dateTimePicker1.Value = DateTime.Now;
             dateTimePicker2.Value = DateTime.Now;
+
+            convertorComboBox.SelectedIndex = 0;
+            dateFormatComboBox.SelectedIndex = 0;
         }
         
         /// <summary>
@@ -1071,33 +1075,6 @@ namespace FluxViewer
             //метод гауса вычисляем значения
         }
 
-        /// <summary>
-        /// изменение даты начала отрезка
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
-        {
-            var beginDate = new DateTime(dateTimePicker2.Value.Year, dateTimePicker2.Value.Month, dateTimePicker2.Value.Day, 00, 00, 00, 000);
-            var endDate = new DateTime(dateTimePicker2.Value.Year, dateTimePicker2.Value.Month, dateTimePicker2.Value.Day, 23, 59, 59, 999);
-
-            comboBox1.Text = "";
-
-            var dataCount = _storage.GetDataCountBetweenTwoDates(beginDate, endDate);
-            if (dataCount != 0)
-            {
-               _baseSize = dataCount;
-               _list.Clear();
-               comboBox1.Enabled = true;
-               btn_export.Enabled = true;
-            }
-            else
-            {
-                comboBox1.Enabled = false;
-                btn_export.Enabled = false;
-            }
-        }
-
 
         /// <summary>
         /// Построить график архива
@@ -1449,36 +1426,6 @@ namespace FluxViewer
             }
         }
 
-        private void btn_export_Click(object sender, EventArgs e)
-        {
-            var saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "CSV|*.csv";
-            saveFileDialog.RestoreDirectory = true;
-            saveFileDialog.CreatePrompt = true;
-            saveFileDialog.Title = "Сохранить выбраный отрезок как";
-
-            if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
-            
-            var beginDate = new DateTime(dateTimePicker1.Value.Year, dateTimePicker1.Value.Month, dateTimePicker1.Value.Day, 00, 00, 00, 000);
-            var endDate = new DateTime(dateTimePicker2.Value.Year, dateTimePicker2.Value.Month, dateTimePicker2.Value.Day, 23, 59, 59, 999);
-            var csvConverter = new CsvConverter(Path.GetFullPath(saveFileDialog.FileName));
-            csvConverter.Open();
-            for (var batchNUmber = 0; ; batchNUmber++)
-            {
-                try
-                {
-                    var dataBatch = _storage.GetDataBatchBetweenTwoDates(beginDate, endDate, batchNUmber);
-                    if (dataBatch.Count != 0)
-                        csvConverter.Write(dataBatch);
-                }
-                catch(Exception ex) // TODO Тут отследить норм. исключение
-                {
-                    break;  
-                }
-            }
-            csvConverter.Close();
-        }
-
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             string mailto = string.Format("mailto:{0}?Subject={1}&Body={2}", "az@azmotors.ru", "Запрос по FluxViewer", "Добрый день!");
@@ -1602,18 +1549,124 @@ namespace FluxViewer
             return matrix;
         }
 
-  /*      private double[] Gauss(double[,] a, int n)
+        private void beginExportDate_ValueChanged(object sender, EventArgs e)
         {
-        double[,] initial_a_matrix;
-        double[,] a_matrix;  // матрица A
-        double[] x_vector;   // вектор неизвестных x
-        double[] initial_b_vector;
-        double[] b_vector;   // вектор b
-        double eps;          // порядок точности для сравнения вещественных чисел 
-        int size;            // размерность задачи
+            var beginDate = new DateTime(beginExportDate.Value.Year, beginExportDate.Value.Month, beginExportDate.Value.Day, 00, 00, 00, 000);
+            var endDate = new DateTime(endExportDate.Value.Year, endExportDate.Value.Month, endExportDate.Value.Day, 23, 59, 59, 999);
+            
+            var dataCount = _storage.GetDataCountBetweenTwoDates(beginDate, endDate);
+            exportDataCountTextBox.Text = $"{dataCount} шт.";
+            exportButton.Enabled = (dataCount != 0);
 
-            return x;
-        }*/
+            var allDatesWithData = _storage.GetAllDatesWithDataBetweenTwoDates(beginDate, endDate);
+            firstExportDateTextBox.Text = allDatesWithData.First().ToString("d");
+            lastExportDateTextBox.Text = allDatesWithData.Last().ToString("d");
+        }
 
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = SaveDialogFilterByExporterType();
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.CreatePrompt = true;
+            saveFileDialog.Title = "Сохранить выбраный отрезок как";
+            if (saveFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+            
+            var exporter = ProvideFileExporterByExporterType(saveFileDialog.FileName);
+
+            var beginDate = new DateTime(beginExportDate.Value.Year, beginExportDate.Value.Month, beginExportDate.Value.Day, 00, 00, 00, 000);
+            var endDate = new DateTime(endExportDate.Value.Year, endExportDate.Value.Month, endExportDate.Value.Day, 23, 59, 59, 999);
+            var allDatesWithData = _storage.GetAllDatesWithDataBetweenTwoDates(beginDate, endDate);
+            
+            var fillHoles = fillHolesCheckBox.Checked;
+            exportProgressBar.Maximum = fillHoles ? (allDatesWithData.Last() - allDatesWithData.First()).Days : allDatesWithData.Count();
+            exportProgressBar.Step = 1;
+            
+            var currentDate = new DateTime(beginDate.Year, beginDate.Month, beginDate.Day, 00, 00, 00);
+            while (currentDate <= endDate)
+            {
+                if (_storage.HasDataForThisDate(currentDate))
+                {
+                    var dataBatch = _storage.GetDataBatchByDate(currentDate);
+                    exporter.Export(fillHoles ? PlaceHolder.FillHoles(dataBatch) : dataBatch);
+                }
+                else
+                {
+                    // Если надо заполнять пробелы и мы находимся в диапазоне дат, в которые прибор записывал
+                    // (чтобы иметь гарантии всегда получить предыдущий и следующий батч данных), то можем генерировать батч
+                    if (fillHoles && currentDate >= allDatesWithData.First() && currentDate <= allDatesWithData.Last())
+                    {
+                        try
+                        {
+                            var prevDataBatch = _storage.GetPrevDataBatchAfterThisDate(currentDate);
+                            var nextDataBatch = _storage.GetNextDataBatchAfterThisDate(currentDate);
+                            var dataBatch = DataBatchGenerator.GenerateDataBatch(
+                                currentDate,
+                                250, // TODO: высчитывать на основе двух батчей
+                                (prevDataBatch.Last().FluxSensorData + nextDataBatch.First().FluxSensorData) / 2,
+                                (prevDataBatch.Last().TempSensorData + nextDataBatch.First().TempSensorData) / 2,
+                                (prevDataBatch.Last().PressureSensorData + nextDataBatch.First().PressureSensorData) / 2,
+                                (prevDataBatch.Last().HumiditySensorData + nextDataBatch.First().HumiditySensorData) / 2
+                            );
+                            exporter.Export(fillHoles ? PlaceHolder.FillHoles(dataBatch) : dataBatch);
+                        }
+                        catch (PrevDataBatchNotFoundException exception)
+                        {
+                            // TODO: в логи, т.к. странная ситуация    
+                        }
+                        catch (NextDataBatchNotFoundException exception)
+                        {
+                            // TODO: в логи, т.к. странная ситуация    
+                        }
+                        
+                    }
+                }
+                
+                currentDate = currentDate.AddDays(1);
+                exportProgressBar.PerformStep();
+            }
+            
+            exportProgressBar.Value = 0;
+        }
+
+        private string SaveDialogFilterByExporterType()
+        {
+            var exporterType = convertorComboBox.SelectedItem.ToString();
+            return exporterType switch
+            {
+                "Plain Text" => "TXT|*.txt",
+                "CSV" => "CSV|*.csv",
+                "JSON" => "JSON|*.json",
+                _ => throw new Exception("Неизвестный тип экспортёра")
+            };
+        }
+        
+        private FileExporter ProvideFileExporterByExporterType(string pathToFile)
+        {
+            var dateTimeFormat = dateFormatComboBox.SelectedItem.ToString();
+            var dateTimeNeedExport = dateTimeForExportCheckBox.Checked;
+            var fluxNeedExport = fluxForExportCheckBox.Checked;
+            var tempNeedExport = tempForExportCheckBox.Checked;
+            var hummNeedExport = hummForExportCheckBox.Checked;
+            var presTimeNeedExport = presForExportCheckBox.Checked;
+
+            var exporterType = convertorComboBox.SelectedItem.ToString();
+            return exporterType switch
+            {
+                "Plain Text" => new PlainTextFileExporter(pathToFile, dateTimeFormat, dateTimeNeedExport,
+                    fluxNeedExport, tempNeedExport, hummNeedExport, presTimeNeedExport),
+                "CSV" => new CsvFileExporter(pathToFile, dateTimeFormat, dateTimeNeedExport, fluxNeedExport,
+                    tempNeedExport, hummNeedExport, presTimeNeedExport),
+                "JSON" => new JsonFileExporter(pathToFile, dateTimeFormat, dateTimeNeedExport, fluxNeedExport,
+                    tempNeedExport, hummNeedExport, presTimeNeedExport),
+                _ => throw new Exception("Неизвестный тип экспортёра")
+            };
+        }
+        
+        private void dateTimeForExportCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dateFormatComboBox.Enabled = dateTimeForExportCheckBox.Checked;
+        }
     }
 }
