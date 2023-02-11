c7827cb28d6e8524158df0c4ee16837b701744d1
artempelevin <78497650+artempelevin@users.noreply.github.com>
2023-02-11 17:42:08 +0300

    feat: Добавлено перечисление типов графика

---
+++ FluxViewer.App/Enums/GraphType.cs
@@ -0,0 +1,36 @@
+﻿namespace FluxViewer.App.Enums;
+
+public enum GraphType
+{
+    FluxGraph,
+    TemperatureGraph,
+    PressureGraph,
+    HumidityGraph,
+}
+
+public static class GraphTypeHelper
+{
+    public static string ToString(GraphType graphType)
+    {
+        return graphType switch
+        {
+            GraphType.FluxGraph => "Электростатическое поле, В/м",
+            GraphType.TemperatureGraph => "Температура, гр. С",
+            GraphType.PressureGraph => "Давление, мм. рт. ст.",
+            GraphType.HumidityGraph => "Влажность, %",
+            _ => throw new ArgumentOutOfRangeException(nameof(graphType), graphType, null)
+        };
+    }
+
+    public static GraphType FromString(string value)
+    {
+        return value switch
+        {
+            "Электростатическое поле, В/м" => GraphType.FluxGraph,
+            "Температура, гр. С" => GraphType.TemperatureGraph,
+            "Давление, мм. рт. ст." => GraphType.PressureGraph,
+            "Влажность, %" => GraphType.HumidityGraph,
+            _ => throw new ArgumentException("Неизвестный тип графика")
+        };
+    }
+}
