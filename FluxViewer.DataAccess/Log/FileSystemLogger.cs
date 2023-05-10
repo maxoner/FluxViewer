957bdcf0241f0c0f59672b2607b10c86ed0dd2e0
artempelevin <78497650+artempelevin@users.noreply.github.com>
2023-05-10 09:59:30 +0300

    feat: Добавлена новая реализация логов, основанная на файлах

---
+++ FluxViewer.DataAccess/Log/FileSystemLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FluxViewer.DataAccess.Log;
+
+public class FileSystemLogger : ILogger
+{
+    private const string FilenameDateFormat = "yyyy_MM_dd";
+    private const string FilenameExtension = "log";
+    
+    public void WriteLog(LogLevel logLevel, LogInitiator logInitiator, string message)
+    {
+        var pathToStorageDir = Path.Combine(Directory.GetCurrentDirectory(), "logs");
+        if (!Directory.Exists(pathToStorageDir))
+            Directory.CreateDirectory(pathToStorageDir);
+        var filename = DateTime.Today.ToString(FilenameDateFormat) + "." + FilenameExtension;
+        var pathToCurrentFile = Path.Combine(pathToStorageDir, filename);
+        
+        using var file = new StreamWriter(pathToCurrentFile);
+        file.Write(MakeLogMessage(logLevel, logInitiator, message));
+    }
+
+    private static string MakeLogMessage(LogLevel logLevel, LogInitiator logInitiator, string message)
+    {
+        return $"[{DateTime.Now} | {logInitiator.ToString()} | {logLevel.ToString()}] {message}";
+    }
+}
---
+++ FluxViewer.DataAccess/Log/ILogger.cs
@@ -0,0 +1,18 @@
+﻿namespace FluxViewer.DataAccess.Log;
+
+
+/// <summary>
+/// Интерфейс взаимодействия с системой логгирования.
+/// В нём доступны методы чтения/записи логов.
+/// </summary>
+public interface ILogger
+{
+    /// <summary>
+    /// Сделать запись лога. 
+    /// </summary>
+    /// <param name="logLevel">Уровень важности лога</param>
+    /// <param name="logInitiator">Инициатор лога</param>
+    /// <param name="message">Сообщение лога</param>
+    void WriteLog(LogLevel logLevel, LogInitiator logInitiator, string message);
+    
+}
