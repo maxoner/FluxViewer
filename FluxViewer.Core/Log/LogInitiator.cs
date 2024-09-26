c66f9dfb2fe041e496d48deeada7e5dbd8a4a490
artempelevin <artempelevin@list.ru>
2024-09-26 23:00:50 +0300

    Добавлен Log

---
+++ FluxViewer.Core/Log/LogInitiator.cs
@@ -0,0 +1,14 @@
+﻿namespace FluxViewer.Core.Log;
+
+public enum LogInitiator
+{
+    /// <summary>
+    /// Инициатор лога - прибор
+    /// </summary>
+    Device = 10,
+
+    /// <summary>
+    /// Инициатор лога - приложение
+    /// </summary>
+    Application = 20
+}
---
+++ FluxViewer.Core/Log/LogLevel.cs
@@ -0,0 +1,29 @@
+﻿namespace FluxViewer.Core.Log;
+
+public enum LogLevel
+{
+    /// <summary>
+    /// Отладочная информация
+    /// </summary>
+    Debug = 10,
+
+    /// <summary>
+    /// Просто информация, требующая фиксации в работе приложения
+    /// </summary>
+    Info = 20,
+
+    /// <summary>
+    /// Важные предупреждения
+    /// </summary>
+    Warning = 30,
+
+    /// <summary>
+    /// Информация об ошибке
+    /// </summary>
+    Error = 40,
+
+    /// <summary>
+    /// Информация о критической ошибки, после которой невозможна корректная работа приложения
+    /// </summary>
+    Critical = 50
+}
