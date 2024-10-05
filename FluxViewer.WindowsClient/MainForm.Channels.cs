510a7ab2544a621168e9434e5a12acb4147e277b
artempelevin <artempelevin@list.ru>
2024-10-05 20:48:10 +0300

    feat(issue-4): Объединение логики отрисовки каналов и логики рассчёта размеров/координат каналов

--- FluxViewer.WindowsClient/MainForm.Channels.cs
+++ FluxViewer.WindowsClient/MainForm.Channels.cs
@@ -1,3 +1,5 @@
+using FluxViewer.Core.Controllers;
+
 namespace FluxViewer.WindowsClient;
 
 /// <summary>
@@ -5,42 +7,46 @@ namespace FluxViewer.WindowsClient;
 /// </summary>
 partial class MainForm
 {
-    // Счетчик для отслеживания количества добавленных форм
-    int formCount = 0;
+    int channelCount; // Счетчик для отслеживания количества добавленных форм
 
     private void addChannelButton_Click(object sender, EventArgs e)
     {
-        // Создаем новую форму ChannelForm
-        Form channelForm = new Form
-        {
-            Name = "form" + formCount,   // Даем уникальное имя
-            TopLevel = false,            // Убираем статус топ-уровневого окна, чтобы встроить форму
-            FormBorderStyle = FormBorderStyle.Sizable, // Можно использовать любые другие стили границы
-            Size = new Size(300, 200),   // Размер встроенной формы
-            Location = new Point(10, formCount * 210), // Позиция в panel2
-            Text = "Канал " + (formCount + 1) // Заголовок формы "Канал 1", "Канал 2" и т.д.
-        };
-
-        // Добавляем форму в panel2
-        mainPanel.Controls.Add(channelForm);
-
-        // Отображаем форму
-        channelForm.Show();
-
-        formCount++;  // Увеличиваем счетчик добавленных форм
+        channelCount++;
+        drawChannels();
     }
 
     private void removeChannelButton_Click(object sender, EventArgs e)
     {
-        if (formCount > 0)
+        if (channelCount > 0)
+            channelCount--;
+        drawChannels();
+    }
+
+    private void drawChannels()
+    {
+        mainPanel.Controls.Clear(); // Удаляем все существующие формы (и ниже рисуем их вновь)
+
+        if (channelCount == 0) // нечего рисовать --> выходим
+            return;
+
+        var windowsInfo = WindowDistributer.DistributeWindows(mainPanel.Width, mainPanel.Height, channelCount);
+        foreach (var windowInfo in windowsInfo)
         {
-            // Находим последнюю добавленную форму
-            Control lastForm = mainPanel.Controls["form" + (formCount - 1)];
+            // Создаем новую форму ChannelForm
+            var channelForm = new Form
+            {
+                Name = "form" + channelCount, // Даем уникальное имя
+                TopLevel = false, // Убираем статус топ-уровневого окна, чтобы встроить форму
+                FormBorderStyle = FormBorderStyle.Sizable, // Можно использовать любые другие стили границы
+                Size = new Size(windowInfo.Width, windowInfo.Height), // Размер встроенной формы
+                Location = new Point(windowInfo.X, windowInfo.Y), // Позиция в panel2
+                Text = "Канал " + (channelCount + 1) // Заголовок формы "Канал 1", "Канал 2" и т.д.
+            };
+            // Добавляем форму в panel2
+            mainPanel.Controls.Add(channelForm);
 
-            // Удаляем последнюю форму
-            mainPanel.Controls.Remove(lastForm);
-
-            formCount--;  // Уменьшаем счетчик
+            // Отображаем форму
+            channelForm.Show();
         }
     }
 }
